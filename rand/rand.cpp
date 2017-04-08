1e33bd061ea4d75f1165402fc5c46ab7dbdb5eaa
muhammss <muhammss@muhammss-laptop.math.ntnu.no>
2017-04-08 17:30:15 +0200

    New Projects added to the repository

---
+++ rand/rand.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+int i,j;
+
+srand((unsinged)time(NULL));
+
+for (i=0; i<10; i++)
+{
+j=rand();
+cout<<"Random number:"<<j<<endl;
+
+}
+return 0;
+}
