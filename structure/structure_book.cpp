551c816437b545f39555a7fead413d769ebf4c07
Salman <muhammad.siddiqui@math.ntnu.no>
2017-04-09 13:40:54 +0200

    structure

---
+++ structure/structure_book.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+ 
+using namespace std;
+
+struct Books {
+char title[50];
+char author[50];
+char subject[50];
+int book_id;
+};
+
+int main()
+{
+struct Books Book1;
+struct Books Book2;
+
